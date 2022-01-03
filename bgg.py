7270af5a5d288b94c86735ac6ee560e91d0cafb3
Zack Amiton <zamiton@outlook.com>
2022-01-03 14:31:29 -0800

    Switch from urwid to simple_term_menu, because urwid is way overkill and doesn't work right

--- bgg.py
+++ bgg.py
@@ -1,11 +1,11 @@
-import urwid
 import link
 import json
 import os
-from sys import argv, platform
+from sys import platform
 import argparse
 import webbrowser
-import shlex
+
+from simple_term_menu import TerminalMenu
 
 cache_path = os.path.join(os.path.dirname(__file__), "cache.json")
 selected = None
@@ -21,23 +21,6 @@ class Reversor:
     def __init__(self, value): self.value =value
     def __eq__(self, oth): return oth.value == self.value
     def __lt__(self, oth): return oth.value < self.value
-
-def input_handler(key):
-    global selected
-
-    key = key.lower()
-    if key in ('q', 'esc'):
-        raise urwid.ExitMainLoop()
-    elif key == 'up':
-        active, idx = listbox.get_focus()
-        listbox.set_focus(idx - 1 if idx > 0 else len(ui_items) - 1)
-    elif key == 'down':
-        active, idx = listbox.get_focus()
-        listbox.set_focus(idx + 1 if idx < len(ui_items) - 1 else 0)
-    elif key == 'enter':
-        active, idx = listbox.get_focus()
-        selected = idx
-        raise urwid.ExitMainLoop()
 
 def choose_title():
     if platform == 'ios':
@@ -90,6 +73,8 @@ if __name__ == '__main__':
             parser.add_argument('-n', '--nocache', action='store_true', help='ignore cache')
             parser.add_argument('-m', '--sortmode', default='plays', const='plays', nargs='?', choices=['title', 'plays'], help='mode to sort summary by')
             
+            parser.add_argument('-c', '--comment', nargs='?', help='play comment')
+
             args = vars(parser.parse_args())
             add, summary = args.get('add'), args.get('summary')
         
@@ -111,23 +96,14 @@ if __name__ == '__main__':
                     if not game_options:
                         print("No items found!")
                     else:
-                        game_items = [f'-> {game["name"]} ({game["year"]}) - ID: {game["idx"]}' for game in game_options]
-
-                        palette = [('reveal focus', 'black', 'dark cyan', 'standout')]
-                        ui_items = [urwid.Text(item) for item in game_items]
-                        content = urwid.SimpleListWalker([urwid.AttrMap(item, None, 'reveal focus') for item in ui_items])
-                        listbox = urwid.ListBox(content)
-                        header = urwid.AttrMap(urwid.Text("Select a BGG Game Item", wrap='clip'), 'header')
-                        top = urwid.Frame(listbox, header)
-                        loop = urwid.MainLoop(top, palette, unhandled_input=input_handler)
-                        loop.run()
-
-                        if isinstance(selected, int): selected = game_options[selected]
-
+                        game_items = [f'{game["name"]} ({game["year"]}) - ID: {game["idx"]}' for game in game_options]
+                        sidx = TerminalMenu(game_items, menu_highlight_style=("bg_cyan", "fg_black")).show()
+                        selected = game_options[sidx] if isinstance(sidx, int) else None
+                        
                 if selected is not None:
                     print(f"Adding {CYAN}{plays} {'plays' if plays > 1 else 'play'}{DEFAULT} to {YELLOW}{selected['name']} ({selected['year']}){DEFAULT}...")
                     try:
-                        if link.log_play(selected['idx'], plays=plays):
+                        if link.log_play(selected['idx'], plays=plays, comment=args.get('comment')):
                             print(f"{GREEN}{'Plays added!' if plays > 1 else 'Play added!'}{DEFAULT}")
                         else:
                             print(f"{RED}Play adding failed!{DEFAULT}")
@@ -142,6 +118,7 @@ if __name__ == '__main__':
                             json.dump(cache, cf)
                     except:
                         print(f"{RED}Play adding failed!{DEFAULT}")
+
             elif summary is not None:
                 days = 0
                 filter_on = "".join(summary[1:]) if len(summary) > 1 else ""
